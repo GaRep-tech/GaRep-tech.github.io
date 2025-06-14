3ae18e07d64845abc94c8609045514e747c80fc5
Galen <galenreposh@gmail.com>
2025-06-14 00:11:53 -0400

    fixed formatting issues in the code

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function reset_animation() {
-  var el = document.getElementById('typed-out');
-  el.style.animation = 'none';
+  var el = document.getElementById("typed-out");
+  el.style.animation = "none";
   el.offsetHeight; /* trigger reflow */
   el.style.animation = null; // reset animation
 }
-function toggleMenu(){
+function toggleMenu() {
   const menu = document.querySelector(".menu-links");
-  const icon= document.querySelector(".hamburger-icon");
+  const icon = document.querySelector(".hamburger-icon");
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
