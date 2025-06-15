1a41ec953fb882fc58c8efd9f3486235c2fce370
Galen <galenreposh@gmail.com>
2025-06-15 19:30:14 -0400

    Chenges typing animation
    Fixed animation and added summary

--- index.js
+++ index.js
@@ -10,3 +10,47 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+/* * Typewriter effect for a list of phrases*/
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+const phrases = [
+  "Database Rules Admin",
+  "Aspiring Web Developer",
+  "Tech Enthusiast",
+];
+const el = document.getElementById("typewriter");
+
+let sleepTime = 100;
+
+let curPhraseIndex = 0;
+
+const writeLoop = async () => {
+  while (true) {
+    let curWord = phrases[curPhraseIndex];
+
+    for (let i = 0; i < curWord.length; i++) {
+      el.innerText = curWord.substring(0, i + 1);
+      await sleep(sleepTime);
+    }
+
+    await sleep(sleepTime * 10);
+
+    for (let i = curWord.length; i > 0; i--) {
+      el.innerText = curWord.substring(0, i - 1);
+      await sleep(sleepTime);
+    }
+
+    await sleep(sleepTime * 5);
+
+    if (curPhraseIndex === phrases.length - 1) {
+      curPhraseIndex = 0;
+    } else {
+      curPhraseIndex++;
+    }
+  }
+};
+
+writeLoop();
